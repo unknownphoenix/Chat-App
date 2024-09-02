1be8b53d7711f6c530e9dc2d8ec43f1bb57cd26d
unknownphoenix <76801603+unknownphoenix@users.noreply.github.com>
2024-09-02 19:36:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # Chat-App
-This is a simple chat application using websockets and npm.
-1. Learning socket.io
-2. getting familiar with gitHub :)
---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const {Server} = require("socket.io")
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    socket.on('user-message', (message) => {
+        io.emit("message",message);
+    });
+    });
+
+app.use(express.static(path.resolve("./public")));
+
+app.get('/', (req, res) => {
+    return res.sendFile("/public/index.html");
+});
+server.listen(9000,() => console.log('Server started at port:9000'));
